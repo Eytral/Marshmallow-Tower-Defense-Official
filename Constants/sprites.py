4afdc29f0ccfcc66394dc1367ea3dcd7310fc890
Eytral <today987789@gmail.com>
2025-03-20 22:51:56 +0800

    base enemy implemented

--- Constants/sprites.py
+++ Constants/sprites.py
@@ -3,3 +3,22 @@ from Constants import config
 # Map Sprites------------------------------------------------------------------------------------------------------------------------
 marsh_mallows_img = pygame.image.load("Assets/Maps/placeholder_map.jpeg")
 MARSH_MALLOWS_SPRITE = pygame.transform.scale(marsh_mallows_img, (config.GRID_SIZE, config.GRID_SIZE))
+
+# Enemy Sprites------------------------------------------------------------------------------------------------------------------------
+enemy_default = pygame.image.load("Assets/Sprites/Enemies/enemy_placeholder_sprite.jpg")
+ENEMY_DEFAULT_SPRITE = pygame.transform.scale(enemy_default, (config.GRID_CELL_SIZE, config.GRID_CELL_SIZE))
+
+marshmallow = pygame.image.load("Assets/Sprites/Enemies/marshmallow.jpg")
+MARSHMALLOW_SPRITE = pygame.transform.scale(marshmallow, (config.GRID_CELL_SIZE, config.GRID_CELL_SIZE))
+
+cracker = pygame.image.load("Assets/Sprites/Enemies/cracker.jpg")
+CRACKER_SPRITE = pygame.transform.scale(cracker, (config.GRID_CELL_SIZE, config.GRID_CELL_SIZE))
+
+white_chocolate = pygame.image.load("Assets/Sprites/Enemies/white_chocolate.jpg")
+WHITE_CHOCOLATE_SPRITE = pygame.transform.scale(white_chocolate, (config.GRID_CELL_SIZE, config.GRID_CELL_SIZE))
+
+dark_chocolate = pygame.image.load("Assets/Sprites/Enemies/dark_chocolate.jpg")
+DARK_CHOCOLATE_SPRITE = pygame.transform.scale(dark_chocolate, (config.GRID_CELL_SIZE, config.GRID_CELL_SIZE))
+
+smore = pygame.image.load("Assets/Sprites/Enemies/smore.jpg")
+SMORE_SPRITE = pygame.transform.scale(smore, (config.GRID_CELL_SIZE, config.GRID_CELL_SIZE))
---
+++ Entities/Enemies/base_enemy.py
@@ -0,0 +1,49 @@
+from Constants import sprites, config
+import copy
+import pygame
+
+class Enemy():
+    def __init__(self, game, start_position, reward=5, health=10, speed=2):
+        self.reward = reward
+        self.max_health = health
+        self.damage = self.max_health//2
+        self.speed = speed
+
+        self.position = copy.deepcopy(start_position)
+        self.width, self.height = config.GRID_CELL_SIZE, config.GRID_CELL_SIZE
+        self.hitbox = pygame.Rect(self.position[0], self.position[1], self.width, self.height)
+
+        self.health = self.max_health
+        self.is_dead = False
+        self.reached_end = False
+        self.sprite = sprites.ENEMY_DEFAULT_SPRITE
+
+        self.game = game
+
+    def move(self):
+        for _ in range(self.speed):
+            # logic for movement
+            self.hitbox = pygame.Rect(self.position[0], self.position[1], config.GRID_CELL_SIZE, config.GRID_CELL_SIZE)
+
+    def draw(self, screen):
+        screen.blit(self.sprite, (self.position))
+
+    def take_damage(self, damage, **kwargs):
+        print(f"Enemy taken {damage} damage")
+        self.health -= damage
+
+    def check_is_dead(self):
+        if self.health <= 0:
+            self.die()
+
+    def die(self):
+        self.is_dead = True
+        print(f"Enemy has reached end (of its life)")
+
+
+    def update(self):
+        self.move()
+        self.check_is_dead()
+        self.check_has_reached_end()
+
+        
