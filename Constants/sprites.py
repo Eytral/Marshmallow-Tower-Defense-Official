57f58c34acbea8c82f1bd2c5f360c9ca65f7021c
Eytral <today987789@gmail.com>
2025-03-21 10:57:45 +0800

    implemented placeholder towers for demonstration purposes - changed jpgs to pngs and renamed files for consistency

--- Constants/sprites.py
+++ Constants/sprites.py
@@ -6,10 +6,10 @@ marsh_mallows_img = pygame.image.load("Assets/Maps/placeholder_map.jpeg")
 MARSH_MALLOWS_SPRITE = pygame.transform.scale(marsh_mallows_img, (config.GRID_SIZE, config.GRID_SIZE))
 
 # Tower Sprites------------------------------------------------------------------------------------------------------------------------
-tower_default = pygame.image.load("Assets/Sprites/Towers/tower_placeholder_sprite.jpg")
+tower_default = pygame.image.load("Assets/Sprites/Towers/tower_placeholder_sprite.png")
 TOWER_DEFAULT_SPRITE = pygame.transform.scale(tower_default, (config.GRID_CELL_SIZE, config.GRID_CELL_SIZE))
 
-birdflamethrower_tower_sprite = pygame.image.load("Assets/Sprites/Towers/birdflamethrower_tower.jpg")
+birdflamethrower_tower_sprite = pygame.image.load("Assets/Sprites/Towers/birdflamethrower_tower.png")
 BIRDFLAMETHROWER_TOWER_SPRITE = pygame.transform.scale(birdflamethrower_tower_sprite, (config.GRID_CELL_SIZE, config.GRID_CELL_SIZE))
 
 bomb_tower_sprite = pygame.image.load("Assets/Sprites/Towers/bomb_tower.png")
@@ -25,25 +25,25 @@ saw_tower_sprite = pygame.image.load("Assets/Sprites/Towers/saw_tower.png")
 SAW_TOWER_SPRITE = pygame.transform.scale(saw_tower_sprite, (config.GRID_CELL_SIZE, config.GRID_CELL_SIZE))
 
 # Enemy Sprites------------------------------------------------------------------------------------------------------------------------
-enemy_default = pygame.image.load("Assets/Sprites/Enemies/enemy_placeholder_sprite.jpg")
+enemy_default = pygame.image.load("Assets/Sprites/Enemies/enemy_placeholder_sprite.png")
 ENEMY_DEFAULT_SPRITE = pygame.transform.scale(enemy_default, (config.GRID_CELL_SIZE, config.GRID_CELL_SIZE))
 
-marshmallow = pygame.image.load("Assets/Sprites/Enemies/marshmallow.jpg")
+marshmallow = pygame.image.load("Assets/Sprites/Enemies/marshmallow.png")
 MARSHMALLOW_SPRITE = pygame.transform.scale(marshmallow, (config.GRID_CELL_SIZE, config.GRID_CELL_SIZE))
 
-cracker = pygame.image.load("Assets/Sprites/Enemies/cracker.jpg")
+cracker = pygame.image.load("Assets/Sprites/Enemies/cracker.png")
 CRACKER_SPRITE = pygame.transform.scale(cracker, (config.GRID_CELL_SIZE, config.GRID_CELL_SIZE))
 
-white_chocolate = pygame.image.load("Assets/Sprites/Enemies/white_chocolate.jpg")
+white_chocolate = pygame.image.load("Assets/Sprites/Enemies/white_chocolate.png")
 WHITE_CHOCOLATE_SPRITE = pygame.transform.scale(white_chocolate, (config.GRID_CELL_SIZE, config.GRID_CELL_SIZE))
 
-dark_chocolate = pygame.image.load("Assets/Sprites/Enemies/dark_chocolate.jpg")
+dark_chocolate = pygame.image.load("Assets/Sprites/Enemies/dark_chocolate.png")
 DARK_CHOCOLATE_SPRITE = pygame.transform.scale(dark_chocolate, (config.GRID_CELL_SIZE, config.GRID_CELL_SIZE))
 
-smore = pygame.image.load("Assets/Sprites/Enemies/smore.jpg")
+smore = pygame.image.load("Assets/Sprites/Enemies/smore.png")
 SMORE_SPRITE = pygame.transform.scale(smore, (config.GRID_CELL_SIZE, config.GRID_CELL_SIZE))
 
 
 # Projectile Sprites------------------------------------------------------------------------------------------------------------------------
-bullet_img = pygame.image.load("Assets/Sprites/Projectiles/bullet.jpg")
+bullet_img = pygame.image.load("Assets/Sprites/Projectiles/bullet.png")
 BULLET_SPRITE = pygame.transform.scale(bullet_img, (config.GRID_CELL_SIZE//3, config.GRID_CELL_SIZE//3))
--- States/game_state.py
+++ States/game_state.py
@@ -1,6 +1,10 @@
 from States.base_state import State 
 from Game.map import Map 
-
+from Entities.Towers.Bird_Flamethrower_tower import BirdFlamethrower
+from Entities.Towers.Bomb_tower import Bomb
+from Entities.Towers.Laser_tower import Laser
+from Entities.Towers.Saw_tower import Saw
+from Entities.Towers.Turret_Tower import Turret
 class Game_State(State):
     """Main game engine - Manages the in-game logic, events, and rendering"""
 
@@ -13,6 +17,11 @@ class Game_State(State):
         """
         super().__init__(game)  # Call the parent State class constructor
         self.map = Map("Marsh_Mallows")
+        self.towers={}
+
+        placeholder_towers = [(0, 0, BirdFlamethrower), (8,2, Bomb), (6,3, Laser), (2,3, Saw), (9,9, Turret)]
+        for tower in placeholder_towers:
+            self.create_tower(*(tower))
 
     def update(self, events):
         """
@@ -31,6 +40,11 @@ class Game_State(State):
             screen: pygame display surface
         """
         self.map.draw(screen) # Draw Map
+        self.draw_towers(screen)
+    
+    def draw_towers(self, screen):
+        for _, tower in self.towers.items():
+            tower.draw(screen)
 
 
     def handle_events(self, events):
@@ -41,3 +55,7 @@ class Game_State(State):
             events: A list of events such as key presses or mouse clicks.
         """
         pass  # Placeholder for event handling logic
+
+    def create_tower(self, grid_x_position, grid_y_position, tower_type):
+        self.towers[(grid_x_position, grid_y_position)] = tower_type(grid_x_position, grid_y_position)
+        self.map.place_tower(grid_x_position,  grid_y_position)
